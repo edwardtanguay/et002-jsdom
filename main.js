849b708ea3c5561ae915b912fabe718d80a54203
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-23 10:09:58 +0200

    button shows console.log

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const messageElem = document.querySelector('.message');
+const changeTextButtonElem = document.querySelector('.changeTextButton');
 
 messageElem.innerText = 'this is from javascript';
+changeTextButtonElem.addEventListener('click', () => {
+	console.log('button was clicked');
+});
